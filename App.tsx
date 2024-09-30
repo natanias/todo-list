266e54ec841db8edfef7581a0086cb502c6e4612
weider <weideritachi@gmail.com>
2024-09-30 15:39:41 -0300

    criaçâo da barra de pesquisa e top bar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 import { CardTask } from "./src/CardTask/carde";
+import BarraPesquisa from "./src/BarraPesquisa/barra";
+import TopBar from "./src/TopAppBar/top";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ alignItems: "center" }}>
+      <TopBar />
+      <BarraPesquisa />
       <CardTask taskTitle={""} taskDescription={""} taskTag={""} />
     </View>
   );
